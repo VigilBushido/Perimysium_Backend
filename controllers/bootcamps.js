f54e06a742d0e6ae24241579a07c3e824a034a6c
VigilBushido <vigilbushido@gmail.com>
2023-11-27 04:56:45 -0500

    initial create

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
@@ -15,9 +17,14 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamp'});
-}
+exports.createBootcamp = async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success: true, 
+        data: bootcamp  
+    });
+};
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -11,7 +11,11 @@ const BootcampSchema = new mongoose.Schema({
     slug: String,
     description: {
         type: String,
-        required: [true, 'Please add description'],
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+      },
+    website: {
+    type: String,
         match: [
             /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
             'Please use a valid URL with HTTP or HTTPS'
@@ -49,49 +53,50 @@ const BootcampSchema = new mongoose.Schema({
         zipcode: String,
         country: String
       },
-      careers: {
-        // Array of strings
-        type: [String],
-        required: true,
-        enum: [
-          'Web Development',
-          'Mobile Development',
-          'UI/UX',
-          'Data Science',
-          'Business',
-          'Other'
-        ]
-      },
-      averageRating: {
-        type: Number,
-        min: [1, 'Rating must be at least 1'],
-        max: [10, 'Rating must can not be more than 10']
-      },
-      averageCost: Number,
-      photo: {
-        type: String,
-        default: 'no-photo.jpg'
-      },
-      housing: {
-        type: Boolean,
-        default: false
-      },
-      jobAssistance: {
-        type: Boolean,
-        default: false
-      },
-      jobGuarantee: {
-        type: Boolean,
-        default: false
-      },
-      acceptGi: {
-        type: Boolean,
-        default: false
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now
-      }
+    careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: [
+        'Software Development',
+        'Web Development',
+        'Mobile Development',
+        'UI/UX',
+        'Data Science',
+        'Business',
+        'Other'
+    ]
+    },
+    averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating must can not be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+    type: String,
+    default: 'no-photo.jpg'
+    },
+    housing: {
+    type: Boolean,
+    default: false
+    },
+    jobAssistance: {
+    type: Boolean,
+    default: false
+    },
+    jobGuarantee: {
+    type: Boolean,
+    default: false
+    },
+    acceptGi: {
+    type: Boolean,
+    default: false
+    },
+    createdAt: {
+    type: Date,
+    default: Date.now
+    }
 });
 
-module.exports = mongoose.model('Bootcmap', BootcampSchema);
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
