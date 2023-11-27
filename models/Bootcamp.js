796ee8d0c8fa4c2263ebde8c954b4a9f8d988440
VigilBushido <vigilbushido@gmail.com>
2023-11-27 02:48:49 -0500

    created model

---
+++ models/Bootcamp.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than  50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add description'],
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+});
