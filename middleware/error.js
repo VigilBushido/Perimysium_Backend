240a390fafb5cc1254140e6ea770f3eb92bd8f03
VigilBushido <vigilbushido@gmail.com>
2023-11-29 04:40:48 -0500

    bad id and status handled

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,21 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
+    let error = { ...err }
+    error.message = err.message;
     // log to console for dev
     console.log(err.stack.red);
 
+    if (err.name === 'CastError') {
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    };
+    //console.log(err.name);
+
     // future I will extend the error class so it includes status codes
-    res.status(err.statusCode || 500).json({
+    res.status(error.statusCode || 500).json({
         success: false, 
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     });
 };
 
