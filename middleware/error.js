dcaa0fb968826aa50e48a046ab9dce4e3c8fce86
VigilBushido <vigilbushido@gmail.com>
2023-11-29 06:50:56 -0500

    validation and dup error added

--- middleware/error.js
+++ middleware/error.js
@@ -4,15 +4,32 @@ const errorHandler = (err, req, res, next) => {
     let error = { ...err }
     error.message = err.message;
     // log to console for dev
-    console.log(err.stack.red);
-
+    //console.log(err.stack.red);
+    //console.log("%j", err);
+    //console.log(JSON.stringify(err, null, 4));
+/*  console.log(JSON.stringify(err.errors, null, 4));
+    const error_messages = Object.values(err.errors).map(val => val.message);
+    console.log(error_messages); */
+    
+    // Mongoose bad Object
     if (err.name === 'CastError') {
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
     };
-    //console.log(err.name);
 
-    // future I will extend the error class so it includes status codes
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+
+    //console.log(err.name);
     res.status(error.statusCode || 500).json({
         success: false, 
         error: error.message || 'Server Error'
