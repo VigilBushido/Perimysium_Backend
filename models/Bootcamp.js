9f49ff54a9a0f65538c1129bc6558cfef8e944e7
VigilBushido <vigilbushido@gmail.com>
2024-01-07 23:58:49 -0500

    delete courses implemented

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -129,6 +129,13 @@ BootcampSchema.pre('save', async function(next) {
     next();
 });
 
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    console.log(`Courses being removed from bootcamps ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
+
 // Reverse populate with virtuals 
 BootcampSchema.virtual('courses', {
     ref: 'Course',
