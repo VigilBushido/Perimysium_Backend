4c5806d8dc0b9dd7e6ae6ae8ce0a0e3afc24d3de
VigilBushido <vigilbushido@gmail.com>
2024-01-09 23:57:50 -0500

    Get a single course

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getCourse } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true }); // we merge the url params from other routes
 
 router.route('/').get(getCourses);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
