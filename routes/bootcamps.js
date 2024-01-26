e07b57f6342871e2de1c44fc13f5b7a018fa9346
VigilBushido <vigilbushido@gmail.com>
2024-01-25 23:29:16 -0500

    built advancedResults middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { getBootcamps, getBootcamp, createBootcamp , updateBootcamp , deleteBootcamp , getBootcampsInRadius , bootcampPhotoUpload } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // include other resource routers
 const courseRouter = require('./courses');
 
@@ -12,7 +15,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
