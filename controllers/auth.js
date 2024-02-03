3d75748d8460b407fe771aa0bbd8949c9a79d4b6
VigilBushido <vigilbushido@gmail.com>
2024-02-02 23:46:32 -0500

    Login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
@@ -15,6 +15,36 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     });
+
+    // Create token
+    const token = user.getSignedJWTToken();
+    res.status(200).json({ succeess: true, token });
+});
+
+// @desc    Login User
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email & password
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email: email }).select('+password');
+
+    if(!user){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check if password matches from the body
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
 
     // Create token
     const token = user.getSignedJWTToken();
