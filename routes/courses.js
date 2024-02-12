b84cd5dd23b259c0a2573b3cc69eca0f8f5d6526
VigilBushido <vigilbushido@gmail.com>
2024-02-12 00:12:24 -0500

    auth protection

--- routes/courses.js
+++ routes/courses.js
@@ -6,12 +6,14 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true }); // we merge the url params from other routes
 
+const { protect } = require('../middleware/auth');
+
 router.route('/').get(advancedResults(Course, {
     path: 'bootcamp',
     select: 'name description'
 }), getCourses)
-.post(addCourse);
+.post(protect, addCourse);
 
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+router.route('/:id').get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
 
 module.exports = router;
