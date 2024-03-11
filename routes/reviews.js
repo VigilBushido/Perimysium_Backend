eebba45265adbd2674a3018557adcfb9eeaf0c76
VigilBushido <vigilbushido@gmail.com>
2024-03-11 01:40:27 -0400

    Get a single review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -13,5 +13,7 @@ router.route('/')
         path: 'bootcamp',
         select: 'name description'
     }), getReviews);
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
