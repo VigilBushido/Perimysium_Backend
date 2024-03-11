e518aa1f5a273aadb115027f4888a3349c4f0773
VigilBushido <vigilbushido@gmail.com>
2024-03-11 00:00:11 -0400

    Reviews implementation

---
+++ models/Review.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a title for the review'],
+        maxlength: 100
+    },
+    text: {
+        type: String,
+        required: [true, 'Please add some text']
+    },
+    weeks: {
+        type: String,
+        required: [true, 'Please add a number of weeks']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
